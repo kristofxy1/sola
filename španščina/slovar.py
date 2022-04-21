2e55a9f30db88b4b50e6fa1a6a662f42b4cde890
Krištof Bučar <kristofxy1@gmail.com>
2022-04-21 15:58:18 +0200

    update

---
+++ španščina/slovar.py
@@ -0,0 +1,26 @@
+seti = {
+    "verbos": "x"
+        }
+
+poskus = {"a": "a",
+        "b": "b"}
+
+verbos = {"abrir (abierto)": "odpreti",
+    "cubrir (cubierto)": "kriti",
+    "descubrir (descubierto)": "odkriti",
+    "morir (muerto)": "umreti",
+    "volver (vuelto)": "vračati se",
+    "devolver (devuelto)": "vrniti",
+    "envolver (envuelto)": "oviti",
+    "resolver (resuelto)": "skleniti",
+    "poner (puesto)": "postaviti",
+    "oponer (opuesto)": "nasprotovati",
+    "componer (copmuesto)": "skladati",
+    "exponer (expuesto)": "razstaviti",
+    "ver (visto)": "videti",
+    "prever (previsto)": "predvidevati",
+    "hacer (hecho)": "delati",
+    "deshacer (deshecho)": "razstaviti",
+    "statisfacer (statisfecho)": "zadovoljiti",
+    "escribir (escrito)": "pisati",
+    "romper (roto)": "lomiti"}
